40351de1d78ecf1e987b0c0b28ceaf9292f27e65
Philip Huang <philip.sw.huang@gmail.com>
2021-05-14 20:57:06 -0700

    navbar added, attempting partials and layouts

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+app.engine = ('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(8080, () => {
+  console.log(`Listening on port 8080!`);
+})
