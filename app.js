5df64c9d27cebb90a5a7525258a55bbae968cec9
Philip Huang <philip.sw.huang@gmail.com>
2021-07-06 16:54:54 -0700

    Added start script

--- app.js
+++ app.js
@@ -281,6 +281,7 @@ app.get('*', (req, res) => {
   res.redirect('/');
 })
 
-app.listen(8080, () => {
-  console.log(`Listening on port 8080!`);
-})
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
